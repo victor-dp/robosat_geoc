be9d915c1f0e5ccecab23af4d2f32a3da4a44092
ocourtin <o@courtin.co>
2018-11-29 19:34:32 +0100

    A bit of abstraction

--- robosat_pink/tools/dedupe.py
+++ robosat_pink/tools/dedupe.py
@@ -12,46 +12,46 @@ from robosat_pink.vectors import make_index, iou
 
 def add_parser(subparser):
     parser = subparser.add_parser(
-        "dedupe", help="deduplicates features against OpenStreetMap", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        "dedupe", help="deduplicates geojson features", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     parser.add_argument(
         "--threshold", type=float, required=True, help="maximum allowed IoU to keep predictions, between 0.0 and 1.0"
     )
-    parser.add_argument("osm", type=str, help="ground truth GeoJSON feature collection from OpenStreetMap")
-    parser.add_argument("predicted", type=str, help="predicted GeoJSON feature collection to deduplicate")
+    parser.add_argument("geojson_a", type=str, help="path to a first GeoJSON file")
+    parser.add_argument("geojson_b", type=str, help="path to a second GeoJSON file")
     parser.add_argument("out", type=str, help="path to GeoJSON to save deduplicated features to")
 
     parser.set_defaults(func=main)
 
 
 def main(args):
-    with open(args.osm) as fp:
-        osm = json.load(fp)
+    with open(args.geojson_a) as fp:
+        features_a = json.load(fp)
 
-    # Todo: at the moment we load all OSM shapes. It would be more efficient to tile
-    #       cover and load only OSM shapes in the tiles covering the predicted shapes.
-    osm_shapes = [shapely.geometry.shape(feature["geometry"]) for feature in osm["features"]]
-    del osm
+    # TODO: at the moment we load all shapes. It would be more efficient to tile
+    #       cover and load only A shapes in the tiles covering the B shapes.
+    features_a = [shapely.geometry.shape(feature["geometry"]) for feature in features_a["features"]]
+    del features_a
 
-    with open(args.predicted) as fp:
-        predicted = json.load(fp)
+    with open(args.geojson_b) as fp:
+        features_b = json.load(fp)
 
-    predicted_shapes = [shapely.geometry.shape(features["geometry"]) for features in predicted["features"]]
-    del predicted
+    features_b_shapes = [shapely.geometry.shape(features["geometry"]) for features in features_b["features"]]
+    del features_b
 
-    idx = make_index(osm_shapes)
+    idx = make_index(features_a_shapes)
     features = []
 
-    for predicted_shape in tqdm(predicted_shapes, desc="Deduplicating", unit="shapes", ascii=True):
-        nearby = [osm_shapes[i] for i in idx.intersection(predicted_shape.bounds, objects=False)]
+    for features_b_shape in tqdm(features_b_shapes, desc="Deduplicating", unit="shapes", ascii=True):
+        nearby = [features_a_shapes[i] for i in idx.intersection(features_b_shape.bounds, objects=False)]
 
         keep = False
 
         if not nearby:
             keep = True
         else:
-            intersecting = [shape for shape in nearby if predicted_shape.intersects(shape)]
+            intersecting = [shape for shape in nearby if features_b_shape.intersects(shape)]
 
             if not intersecting:
                 keep = True
@@ -62,8 +62,7 @@ def main(args):
                     keep = True
 
         if keep:
-            feature = geojson.Feature(geometry=shapely.geometry.mapping(predicted_shape))
-            features.append(feature)
+            features.append(geojson.Feature(geometry=shapely.geometry.mapping(features_b_shape)))
 
     collection = geojson.FeatureCollection(features)
 
