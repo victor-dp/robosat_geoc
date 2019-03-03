8069f41433ee2324d4c946184738cadcb0cb781c
Olivier Courtin <o@datapink.com>
2019-03-03 22:23:34 +0000

    pillow-simd as first choice. Specify minimal versions on several dependancies. Remove dev dependencies (handled in Makefile rather by Pypi). Black session on setup.py

--- setup.py
+++ setup.py
@@ -7,47 +7,58 @@ Semantic Segmentation ecosystem for GeoSpatial Imagery
 """
 
 from setuptools import setup, find_packages
+from pkg_resources import get_distribution
 from os import path
 
 here = path.dirname(__file__)
 
-with open(path.join(here, 'VERSION')) as f:
+with open(path.join(here, "VERSION")) as f:
     version = f.read()
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt')) as f:
+
+with open(path.join(here, "requirements.txt")) as f:
+
+    def is_installed(package):
+        try:
+            get_distribution(package)
+            return True
+        except:
+            return False
+
     install_requires = f.read().splitlines()
+    if is_installed("pillow") and not is_installed("pillow-simd"):
+        install_requires = [require.replace("pillow-simd", "pillow") for require in install_requires]
 
 setup(
-    name='RoboSat.pink',
+    name="RoboSat.pink",
     version=version,
-    url='https://github.com/datapink/robosat.pink',
-    license='MIT',
-    author='DataPink',
-    author_email='robosat@datapink.com',
-    description='Semantic Segmentation ecosystem for GeoSpatial Imagery',
+    url="https://github.com/datapink/robosat.pink",
+    license="MIT",
+    maintainer="DataPink",
+    maintainer_email="robosat@datapink.com",
+    description="Semantic Segmentation ecosystem for GeoSpatial Imagery",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests"]),
     install_requires=install_requires,
-    entry_points = { 'console_scripts': [ 'rsp = robosat_pink.tools.__main__:main' ] },
+    entry_points={"console_scripts": ["rsp = robosat_pink.tools.__main__:main"]},
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-        'Topic :: Scientific/Engineering :: GIS',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
     ],
-
 )
