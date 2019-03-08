2739da19f49fc74be95eed4470117e7150ad2d98
Olivier Courtin <o@datapink.com>
2019-03-08 12:46:19 +0000

    Refactor code and cleanup. Improve CLI response when rsp called without argv. Cf #21

--- robosat_pink/tools/__main__.py
+++ robosat_pink/tools/__main__.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 import sys
 import argparse
@@ -11,23 +9,25 @@ import robosat_pink.tools
 
 
 def main():
+
+    if not len(sys.argv) > 1:
+        print("rsp: RoboSat.pink command line tools")
+        print("Usages:")
+        print("rsp -h, --help            show tools availables")
+        print("rsp <tool> -h, --help     show options for a tool")
+        print("rsp <tool> [...]          launch an rsp tool command")
+        sys.exit()
+
+    path = os.path.dirname(robosat_pink.tools.__file__)
+    tools = [tool for tool in [name for _, name, _ in pkgutil.iter_modules([path]) if name != "__main__"]]
+    tools = [sys.argv[1]] if sys.argv[1] in tools else tools
+
     parser = argparse.ArgumentParser(prog="rsp")
     subparser = parser.add_subparsers(title="RoboSat.pink tools", metavar="")
 
-    try:
-        module = import_module("robosat_pink.tools.{}".format(sys.argv[1]))
+    for tool in tools:
+        module = import_module("robosat_pink.tools.{}".format(tool))
         module.add_parser(subparser)
-    except:
-        path = os.path.dirname(robosat_pink.tools.__file__)
 
-        for tool in [name for _, name, _ in pkgutil.iter_modules([path]) if name != "__main__"]:
-            module = import_module("robosat_pink.tools.{}".format(tool))
-            module.add_parser(subparser)
-
-    subparser.required = True
     args = parser.parse_args()
     args.func(args)
-
-
-if __name__ == "__main__":
-    main()
