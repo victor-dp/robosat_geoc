9b17ef650fba4e36294b4bc3774a58d02341b1dd
wucangeo <wucangeo@gmail.com>
2019-10-09 23:04:59 +0800

    添加subset,train, train待解决

---
+++ test_train.py
@@ -0,0 +1,84 @@
+from robosat_pink.tools import params, cover, download, rasterize, subset, train
+import shutil
+import sys
+import os
+
+if __name__ == "__main__":
+    # example
+    # rsp cover --bbox 4.795,45.628,4.935,45.853 --zoom 18 ds/cover
+    # rsp download --type WMS 'https://download.data.grandlyon.com/wms/grandlyon?SERVICE=WMS&REQUEST=GetMap&VERSION=1.3.0&LAYERS=Ortho2015_vue_ensemble_16cm_CC46&WIDTH=512&HEIGHT=512&CRS=EPSG:3857&BBOX={xmin},{ymin},{xmax},{ymax}&FORMAT=image/jpeg' ds/cover ds/images
+    # rsp rasterize --config ds/config.toml --type Building --geojson ds/lyon_roofprint.json --cover ds/cover ds/labels
+    # rsp cover --dir ds/images --splits 70/30 ds/training/cover ds/validation/cover
+
+    # clear ds directory at first
+    # if os.path.isdir('./ds'):
+    #     shutil.rmtree("./ds")
+
+    # params_cover = params.Cover(
+    #     # bbox="4.842052459716797,45.76126587962455,4.854111671447754,45.770336923575734",
+    #     bbox="118.085517,24.465897,118.124342,24.499751",
+    #     zoom=18, out=["ds/cover"])
+    # cover.main(params_cover)
+
+    # params_download = params.Download(
+    #     type="XYZ",
+    #     url="http://127.0.0.1:7001/wmts/{z}/{x}/{y}?type=google",
+    #     cover="ds/cover",
+    #     out="ds/images")
+    # download.main(params_download)
+
+    # params_rasterize = params.Rasterize(
+    #     config="data/config.toml",
+    #     type="Building",
+    #     ts=256,
+    #     geojson=["data/buildings.json"],
+    #     cover="ds/cover",
+    #     out="ds/labels"
+    # )
+    # rasterize.main(params_rasterize)
+
+    # params_cover2 = params.Cover(
+    #     dir='ds/images',
+    #     splits='70/30',
+    #     out=['ds/training/cover', 'ds/validation/cover']
+    # )
+    # cover.main(params_cover2)
+
+    # params_subset_train_images = params.Subset(
+    #     dir='ds/images',
+    #     cover='ds/training/cover',
+    #     out='ds/training/images'
+    # )
+    # subset.main(params_subset_train_images)
+
+    # params_subset_train_labels = params.Subset(
+    #     dir='ds/labels',
+    #     cover='ds/training/cover',
+    #     out='ds/training/labels'
+    # )
+    # subset.main(params_subset_train_labels)
+
+    # params_subset_validation_images = params.Subset(
+    #     dir='ds/images',
+    #     cover='ds/validation/cover',
+    #     out='ds/validation/images'
+    # )
+    # subset.main(params_subset_validation_images)
+
+    # params_subset_validation_labels = params.Subset(
+    #     dir='ds/labels',
+    #     cover='ds/validation/cover',
+    #     out='ds/validation/labels'
+    # )
+    # subset.main(params_subset_validation_labels)
+
+    params_train = params.Train(
+        config='data/config.toml',
+        epochs=10,
+        ts=(256, 256),
+        dataset='ds',
+        out='ds/model'
+    )
+    train.main(params_train)
+
+    sys.exit(0)
