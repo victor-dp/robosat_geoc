9a300bb8833d08c369a6a9749ea8146843eb9387
liii18 <1262263306@qq.com>
2019-10-12 18:14:57 +0800

    update test_predict

--- test_predict.py
+++ test_predict.py
@@ -1,45 +1,38 @@
 from robosat_pink.tools import params, cover, download, rasterize, predict, vectorize
 
 if __name__ == "__main__":
-    # # rsp cover --bbox 4.795,45.628,4.935,45.853 --zoom 18 ds/cover 4.842052459716797,45.76126587962455,4.854111671447754,45.770336923575734
+    # rsp cover --bbox 4.795,45.628,4.935,45.853 --zoom 18 ds/cover 4.842052459716797,45.76126587962455,4.854111671447754,45.770336923575734
+    # rsp download --type XYZ 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' dp/cover dp/images
+    # rsp rasterize --config data/config.toml --type Building --geojson data/buildings.json  --cover dp/cover dp/labels
+    # rsp predict --checkpoint data/model/checkpoint-0.00005.pth ds dp/masks
+    # rsp vectorize --type --config masks out
+
     # params_cover = params.Cover(
-    #     bbox="4.842052459716797,45.76126587962455,4.854111671447754,45.770336923575734",
-    #     zoom=17, out="ds/cover")
+    #     # bbox="4.842052459716797,45.76126587962455,4.854111671447754,45.770336923575734",
+    #     bbox="119.29679274559021,26.06680633905522,119.30007576942445,26.070054139449656",
+    #     zoom=17, out=["dp/cover"])
     # cover.main(params_cover)
 
-    # # rsp download --type XYZ 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' ds/cover ds/images
     # params_download = params.Download(
     #     type="XYZ",
     #     url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-    #     cover="ds/cover",
-    #     out="ds/images",
+    #     cover="dp/cover",
+    #     out="dp/images",
     #     timeout=20)
     # download.main(params_download)
 
-    # # rsp rasterize --config data/config.toml --type Building --geojson data/buildings.json  --cover ds/cover ds/labels
-    # params_rasterize = params.Rasterize(
-    #     config="data/config.toml",
-    #     type="Building",
-    #     geojson=["data/buildings.json"],
-    #     cover="ds/cover",
-    #     out="ds/labels"
-    # )
-    # rasterize.main(params_rasterize)
-
-    # # rsp predict --checkpoint data/model/checkpoint-0.00005.pth ds ds/masks
     # params_predict = params.Predict(
-    #     dataset="ds",
+    #     dataset="dp",
     #     checkpoint='data/model/checkpoint-00010.pth',
     #     config="data/config.toml",
-    #     out="ds/masks"
+    #     out="dp/masks"
     # )
     # predict.main(params_predict)
 
-    # rsp vectorize --type --config masks out
     params_vectorize = params.Vectorize(
-        masks="ds/masks",
+        masks="dp/masks",
         type="Building",
         config="data/config.toml",
-        out="ds/vectors"
+        out="dp/vectors.json"
     )
     vectorize.main(params_vectorize)
