5e77126eae2a210509da326cfff40d7dc7d5c137
wucangeo <wucangeo@gmail.com>
2019-10-18 19:19:01 +0800

    改造main为入口，合并外部调用

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-pg = "host='172.16.100.140' dbname='tdt2018' user='postgres' password='postgres'"
-# pg = "host='localhost' dbname='tdt2018' user='postgres' password='postgres'"
+# pg = "host='172.16.100.140' dbname='tdt2018' user='postgres' password='postgres'"
+POSTGRESQL = "host='localhost' dbname='tdt2018' user='postgres' password='postgres'"
 
-building_table = "BUIA"
+BUILDING_TABLE = "BUIA"
+
+WMTS_HOST = "http://localhost:5000/v1/wmts"
